f651fc48cb2839f253a303f03e8b6e0b121ebba8
AkashAnthony2001 <akashanthony18@gmail.com>
2023-02-08 16:33:23 +0530

    ts

--- Forms-Project/src/app/signup/signup.component.ts
+++ Forms-Project/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,15 +7,21 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  
-places = ["Andhra Pradesh" , ""]
-firstname = new FormControl('',[Validators.required]);
-lastname = new FormControl('',[Validators.required]);
-email = new FormControl('',[Validators.required,Validators.email]);
-password = new FormControl('',[Validators.required,]);
-confirm = new FormControl('',[Validators.required]);
-gender = new FormControl('',Validators.required);
-phone = new FormControl('',[Validators.required]);
-agree = new FormControl('',Validators.required);
+  places = ["AndhraPradesh","ArunachalPradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","HimachalPradesh","Jharkhand","Karnataka","Kerala","MadhyaPradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","TamilNadu","Telangana","Tripura","UttarPradesh","Uttarakhand","WestBengal"]
 
+
+
+  signup !: FormGroup;
+ngOnInit(){
+  this.signup=new FormGroup({
+'firstname' : new FormControl('',[Validators.required]);
+'lastname' : new FormControl('',[Validators.required]);
+'email' : new FormControl('',[Validators.required,Validators.email]);
+'password' : new FormControl('',[Validators.required,]);
+'confirm' : new FormControl('',[Validators.required]);
+'gender' : new FormControl('',Validators.required);
+'phone' : new FormControl('',[Validators.required]);
+'agree' : new FormControl('',Validators.required);
+  });
+}
 }
